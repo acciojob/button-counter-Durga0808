6cc16186d40e0b3d0664df4918625a567e096c32
Durga0808 <bejawada.durgaprasad@gmail.com>
2024-05-28 21:44:54 +0530

    first commit

---
+++ src/components/ClickCounter.js
@@ -0,0 +1,19 @@
+// src/ClickCounter.js
+import React, { useState } from 'react';
+
+const ClickCounter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Button clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default ClickCounter;
